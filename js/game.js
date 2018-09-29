1490dc5b3d71ea7cf11ce0daecb62b6f072f49bd
Martin Tudor <martcan27@gmail.com>
2018-09-29 19:33:28 +0100

    Remove unnecessary global nextArray

--- js/game.js
+++ js/game.js
@@ -1,205 +1,204 @@
 /* jslint browser: true*/
 /* global  $*/
 $(function() {
-  'use strict';
-  let nextArray = [];
-  // make an array of arrays that will
-  // (correspond to the grid that the user sees)
-  // and fill them with zeroes
-  /**
-   * @param {number} rowWidth
-   * @param {number} columnHeight
-   * @return {array}
-   */
-  function arrayGrid(rowWidth, columnHeight) {
-    let arr = [];
-    let i = 0;
-    while (i < rowWidth) {
-      arr[i] = Array(columnHeight).fill(0);
-      i += 1;
-    }
-    return arr;
-  }
-  let arr = arrayGrid(20, 20);
-  // the grid table
-  const PIXELCANVAS = $('#pixel_canvas');
-  const beginClick = $('#begin');
-  beginClick.on('click', function() {
-    lifeOrDeath();
-  });
-  // determine wether mouse is down or not
-  let mouseDown = false;
-  PIXELCANVAS.on('mousedown', function() {
-    mouseDown = true;
-    return false;
-  });
-  $('body').on('mouseup', function() {
-    mouseDown = false;
-  });
-  // makeGrid function then call it
-  /**
-   * @param  {number} rowWidth
-   * @param  {number} columnHeight
-   * @return {undefined}
-   */
-  function makeGrid(rowWidth, columnHeight) {
-    // remove old grid (if any)
-    PIXELCANVAS.children().remove();
-    // build grid
-    let i2;
-    let str = '';
-    for (i2 = 0; i2 < rowWidth; i2 += 1) {
-      str += '<tr>';
-      let i;
-      for (i = 0; i < columnHeight; i += 1) {
-        // use the arr array to determine
-        // what cells are red. 1 is red(alive). 0 is dead.
-        if (arr[i2][i] === 1) {
-          str += `<td class="cell-${i2}-${i}- coloured">1</td>`;
-        } else {
-          str += `<td class="cell-${i2}-${i}- uncoloured">0</td>`;
+    "use strict";
+    // make an array of arrays that will
+    // (correspond to the grid that the user sees)
+    // and fill them with zeroes
+    /**
+     * @param {number} rowWidth
+     * @param {number} columnHeight
+     * @return {array}
+     */
+    function arrayGrid(rowWidth, columnHeight) {
+        let arr = [];
+        let i = 0;
+        while (i < rowWidth) {
+            arr[i] = Array(columnHeight).fill(0);
+            i += 1;
         }
-      }
-      str += '</tr>';
+        return arr;
     }
-    PIXELCANVAS.append(str);
-  }
-  makeGrid(20, 20);
-  // use split method to grap the class of the clicked cell -
-  // which contains the rowWidth and columnHeight number separated by "-".
-  const updateArray = function(e) {
-    const position2 = $(e.target).attr('class');
-    const splitPosition = position2.split('-');
-    return splitPosition;
-  };
-
-  // paint when a cell is clicked
-  PIXELCANVAS.on('click', 'td', function(e) {
-    const CURRENT_COLOR = $(e.target).css('background-color');
-
-    // current cell will change color
-
-    if (CURRENT_COLOR === 'rgba(0, 0, 0, 0)') {
-      $(e.target)
-          .css('background-color', 'rgb(220, 0, 0)')
-          .css('color', 'rgb(220, 0, 0)')
-          .text('1');
-      const position = updateArray(e);
-      const rowWidth = position[1];
-      const columnHeight = position[2];
-      arr[rowWidth][columnHeight] = 1;
-      // makeGrid(20,20);
-    } else {
-      $(e.target)
-          .css('background-color', 'rgba(0, 0, 0, 0)')
-          .css('color', 'rgba(0, 0, 0, 0)')
-          .text('0');
-      const position = updateArray(e);
-      const rowWidth = position[1];
-      const columnHeight = position[2];
-      arr[rowWidth][columnHeight] = 0;
-      // makeGrid(20,20);
-    }
-  });
-  // paint when mouse held down
-  PIXELCANVAS.on('mouseenter', 'td', function(e) {
-    if (mouseDown) {
-      $(e.target)
-          .css('background-color', 'rgb(220, 0, 0)')
-          .css('color', 'rgb(220, 0, 0)')
-          .text('1');
-      const position = updateArray(e);
-      const rowWidth = position[1];
-      const columnHeight = position[2];
-      arr[rowWidth][columnHeight] = 1;
-    }
-  });
-  // make a copy of an array that has arrays inside it
-  /**
-   * @param  {array} arrayToCopy
-   * @return {array}
-   */
-  function copyArray(arrayToCopy) {
-    const copy = arrayToCopy.map((elem) => {
-      return elem.slice();
+    let arr = arrayGrid(20, 20);
+    // the grid table
+    const PIXELCANVAS = $("#pixel_canvas");
+    const beginClick = $("#begin");
+    beginClick.on("click", function() {
+        lifeOrDeath();
     });
-    return copy;
-  }
-  // life or death?
-  /**
-   */
-  function lifeOrDeath() {
-    // nextArray will be the next state of arr as determined by the rules
-    nextArray = copyArray(arr);
-    let i;
-    for (i = 0; i <= 19; i += 1) {
-      let j;
-      for (j = 0; j <= 19; j += 1) {
-        // determine the surroundings of the cell (the arr Array).
-        // 0 is dead. 1 is alive.
-        // if a cell is at the edge, check the opposite side
-        const top = i === 0 ? arr[19][j] : arr[i - 1][j];
-        const topRight =
-          i === 0 && j === 19
-            ? arr[19][0]
-            : i === 0 && j !== 19
-              ? arr[19][j + 1]
-              : i !== 0 && j === 19
-                ? arr[i - 1][0]
-                : arr[i - 1][j + 1];
-
-        const right = j === 19 ? arr[i][0] : arr[i][j + 1];
-
-        const bottomRight =
-          i === 19 && j === 19
-            ? arr[0][0]
-            : i !== 19 && j === 19
-              ? arr[i + 1][0]
-              : i === 19 && j !== 19
-                ? arr[0][j + 1]
-                : arr[i + 1][j + 1];
-
-        const bottom = i === 19 ? arr[0][j] : arr[i + 1][j];
-
-        const bottomLeft =
-          i === 19 && j === 0
-            ? arr[0][19]
-            : i !== 19 && j === 0
-              ? arr[i + 1][19]
-              : i === 19 && j !== 0
-                ? arr[0][j - 1]
-                : arr[i + 1][j - 1];
-
-        const left = j === 0 ? arr[i][19] : arr[i][j - 1];
-
-        const topLeft =
-          i === 0 && j === 0
-            ? arr[19][19]
-            : i !== 0 && j === 0
-              ? arr[i - 1][19]
-              : i === 0 && j !== 0
-                ? arr[19][j - 1]
-                : arr[i - 1][j - 1];
-        const sum =
-          top +
-          topRight +
-          right +
-          bottomRight +
-          bottom +
-          bottomLeft +
-          left +
-          topLeft;
-        // apply the rules of the Conway's game of life.
-        // https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
-
-        if (sum === 3) {
-          nextArray[i][j] = 1;
-        } else if (sum !== 2) {
-          nextArray[i][j] = 0;
+    // determine wether mouse is down or not
+    let mouseDown = false;
+    PIXELCANVAS.on("mousedown", function() {
+        mouseDown = true;
+        return false;
+    });
+    $("body").on("mouseup", function() {
+        mouseDown = false;
+    });
+    // makeGrid function then call it
+    /**
+     * @param  {number} rowWidth
+     * @param  {number} columnHeight
+     * @return {undefined}
+     */
+    function makeGrid(rowWidth, columnHeight) {
+        // remove old grid (if any)
+        PIXELCANVAS.children().remove();
+        // build grid
+        let i2;
+        let str = "";
+        for (i2 = 0; i2 < rowWidth; i2 += 1) {
+            str += "<tr>";
+            let i;
+            for (i = 0; i < columnHeight; i += 1) {
+                // use the arr array to determine
+                // what cells are red. 1 is red(alive). 0 is dead.
+                if (arr[i2][i] === 1) {
+                    str += `<td class="cell-${i2}-${i}- coloured">1</td>`;
+                } else {
+                    str += `<td class="cell-${i2}-${i}- uncoloured">0</td>`;
+                }
+            }
+            str += "</tr>";
         }
-      }
+        PIXELCANVAS.append(str);
     }
-    arr = copyArray(nextArray);
     makeGrid(20, 20);
-  }
+    // use split method to grap the class of the clicked cell -
+    // which contains the rowWidth and columnHeight number separated by "-".
+    const updateArray = function(e) {
+        const position2 = $(e.target).attr("class");
+        const splitPosition = position2.split("-");
+        return splitPosition;
+    };
+
+    // paint when a cell is clicked
+    PIXELCANVAS.on("click", "td", function(e) {
+        const CURRENT_COLOR = $(e.target).css("background-color");
+
+        // current cell will change color
+
+        if (CURRENT_COLOR === "rgba(0, 0, 0, 0)") {
+            $(e.target)
+                .css("background-color", "rgb(220, 0, 0)")
+                .css("color", "rgb(220, 0, 0)")
+                .text("1");
+            const position = updateArray(e);
+            const rowWidth = position[1];
+            const columnHeight = position[2];
+            arr[rowWidth][columnHeight] = 1;
+            // makeGrid(20,20);
+        } else {
+            $(e.target)
+                .css("background-color", "rgba(0, 0, 0, 0)")
+                .css("color", "rgba(0, 0, 0, 0)")
+                .text("0");
+            const position = updateArray(e);
+            const rowWidth = position[1];
+            const columnHeight = position[2];
+            arr[rowWidth][columnHeight] = 0;
+            // makeGrid(20,20);
+        }
+    });
+    // paint when mouse held down
+    PIXELCANVAS.on("mouseenter", "td", function(e) {
+        if (mouseDown) {
+            $(e.target)
+                .css("background-color", "rgb(220, 0, 0)")
+                .css("color", "rgb(220, 0, 0)")
+                .text("1");
+            const position = updateArray(e);
+            const rowWidth = position[1];
+            const columnHeight = position[2];
+            arr[rowWidth][columnHeight] = 1;
+        }
+    });
+    // make a copy of an array that has arrays inside it
+    /**
+     * @param  {array} arrayToCopy
+     * @return {array}
+     */
+    function copyArray(arrayToCopy) {
+        const copy = arrayToCopy.map((elem) => {
+            return elem.slice();
+        });
+        return copy;
+    }
+    // life or death?
+    /**
+     */
+    function lifeOrDeath() {
+        // nextArray will be the next state of arr as determined by the rules
+        let nextArray = copyArray(arr);
+        let i;
+        for (i = 0; i <= 19; i += 1) {
+            let j;
+            for (j = 0; j <= 19; j += 1) {
+                // determine the surroundings of the cell (the arr Array).
+                // 0 is dead. 1 is alive.
+                // if a cell is at the edge, check the opposite side
+                const top = i === 0 ? arr[19][j] : arr[i - 1][j];
+                const topRight =
+                    i === 0 && j === 19
+                        ? arr[19][0]
+                        : i === 0 && j !== 19
+                            ? arr[19][j + 1]
+                            : i !== 0 && j === 19
+                                ? arr[i - 1][0]
+                                : arr[i - 1][j + 1];
+
+                const right = j === 19 ? arr[i][0] : arr[i][j + 1];
+
+                const bottomRight =
+                    i === 19 && j === 19
+                        ? arr[0][0]
+                        : i !== 19 && j === 19
+                            ? arr[i + 1][0]
+                            : i === 19 && j !== 19
+                                ? arr[0][j + 1]
+                                : arr[i + 1][j + 1];
+
+                const bottom = i === 19 ? arr[0][j] : arr[i + 1][j];
+
+                const bottomLeft =
+                    i === 19 && j === 0
+                        ? arr[0][19]
+                        : i !== 19 && j === 0
+                            ? arr[i + 1][19]
+                            : i === 19 && j !== 0
+                                ? arr[0][j - 1]
+                                : arr[i + 1][j - 1];
+
+                const left = j === 0 ? arr[i][19] : arr[i][j - 1];
+
+                const topLeft =
+                    i === 0 && j === 0
+                        ? arr[19][19]
+                        : i !== 0 && j === 0
+                            ? arr[i - 1][19]
+                            : i === 0 && j !== 0
+                                ? arr[19][j - 1]
+                                : arr[i - 1][j - 1];
+                const sum =
+                    top +
+                    topRight +
+                    right +
+                    bottomRight +
+                    bottom +
+                    bottomLeft +
+                    left +
+                    topLeft;
+                // apply the rules of the Conway's game of life.
+                // https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+
+                if (sum === 3) {
+                    nextArray[i][j] = 1;
+                } else if (sum !== 2) {
+                    nextArray[i][j] = 0;
+                }
+            }
+        }
+        arr = copyArray(nextArray);
+        makeGrid(20, 20);
+    }
 });
