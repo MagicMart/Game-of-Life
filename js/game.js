4424b926ce5c6140f1930bdb638c361d1fbc83f9
Martin Tudor <martcan27@gmail.com>
2019-10-08 21:26:28 +0100

    Update JsDocs

--- js/game.js
+++ js/game.js
@@ -14,6 +14,10 @@ $(function() {
     );
 
     const matrix = (function() {
+        /**
+         * @param {number} size
+         * @returns {Array<Array<number>>}
+         */
         function makeMatrix(size) {
             const row = new Array(size).fill(0);
             return new Array(size).fill(row);
@@ -25,6 +29,10 @@ $(function() {
         function data() {
             return dataMatrix;
         }
+        /**
+         *
+         * @param {Array<Array<number>>} matrix
+         */
         function update(matrix) {
             dataMatrix = matrix;
         }
@@ -35,8 +43,7 @@ $(function() {
     })();
 
     /**
-     *
-     * returns a copy of the matrix
+     * @param {Array<Array<number>>} matrix
      */
     function copyMatrix(matrix) {
         return matrix.map(row => row.map(col => col));
@@ -58,7 +65,7 @@ $(function() {
          * Grabs the text content from the hidden span element:
          * the coordinate of the table cell clicked on.
          * @param {Object} cell event.target
-         * @return {Array} [row, column]
+         * @returns {Array<number>} [row, column]
          */
         const cellCoord = function(cell) {
             const coord = cell.querySelector(".coord");
@@ -110,9 +117,13 @@ $(function() {
         return pixelCanvas;
     })();
 
+    /**
+     * @param {Array<Array<number>>} matrix
+     */
     function buildTable(matrix) {
         matrixNode.children().remove();
         return matrixNode.append(
+            // @ts-ignore
             matrix.map(
                 (row, i) =>
                     "<tr>" +
@@ -126,30 +137,45 @@ $(function() {
             )
         );
     }
-
+    /**
+     *
+     * @param {Array<Array<number>>} matrix
+     */
     function displayMatrix(matrix) {
         const td = Array.from(document.querySelectorAll("td"));
-
+        /**
+         * @type {Array<number>}
+         */
         const flatMatrix = matrix.reduce((acc, current) => {
             return acc.concat(current);
         });
+
         flatMatrix.forEach((el, i) =>
             el === 1
                 ? (td[i].style.backgroundColor = alive)
                 : (td[i].style.backgroundColor = dead)
         );
     }
-
+    /**
+     *
+     * @param {Array<Array<number>>} dataMatrix
+     */
     function lifeOrDeath(dataMatrix) {
         const size = dataMatrix[0].length - 1;
-
+        /**
+         *
+         * @param {Array<Array<number>>} matrix
+         */
         function copyMatrix(matrix) {
             return matrix.map(row => row.map(col => col));
         }
 
         /**check to see if the row or column coordinate
          * is outside the matrix. If it is,
-         * it evaluates to the opposite side of the matrix  */
+         * it evaluates to the opposite side of the matrix
+         * @param {number} num
+         * @returns {number} size or 0 or original number
+         */
         function checkEdge(num) {
             if (num < 0) {
                 num = size;
@@ -249,14 +275,20 @@ $(function() {
         ticker(ticking);
     });
 
-    $("#myRange").on("change", event => {
-        const sliderValue = Number(event.target.value);
-        if (ticking) {
-            ticker(false);
-            tickSpeed = 600 - sliderValue;
-            ticker(true);
-        } else {
-            tickSpeed = 600 - sliderValue;
+    $("#myRange").on(
+        "change",
+        /**
+         * @param {Object} event
+         */
+        event => {
+            const sliderValue = Number(event.target.value);
+            if (ticking) {
+                ticker(false);
+                tickSpeed = 600 - sliderValue;
+                ticker(true);
+            } else {
+                tickSpeed = 600 - sliderValue;
+            }
         }
-    });
+    );
 });
